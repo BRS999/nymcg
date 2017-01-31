535b4639ab9b4ca83873edd9a7ded9d93a33eff2
bspencer <bspencer@conductor.com>
2017-01-31 16:28:08 -0500

    proj

--- Delegates/Functions.cs
+++ Delegates/Functions.cs
@@ -5,16 +5,17 @@ using System.Text.RegularExpressions;
 using System.Net;
 using System.Threading.Tasks;
 
-public class Func {
+public class Func
+{
+    public static void getFunc()
+    {
 
-  public static void getFunc() {
-    
-    // Func types
-    Func<int, bool> even = (x) => x % 2 == 0;
-    
-    //Create a list with only odd numbers
-    var arr = Enumerable.Range(1, 100).Where(even).ToList();
-    
-    arr.ForEach(Console.Write);
-  }
+        // Func types
+        Func<int, bool> even = (x) => x % 2 == 0;
+
+        //Create a list with only odd numbers
+        var arr = Enumerable.Range(1, 100).Where(even).ToList();
+
+        arr.ForEach(Console.Write);
+    }
 }
--- Multithreading/Threading.md
+++ Multithreading/Threading.md
@@ -1,7 +1,6 @@
 
 # Notes from the Meetup
 
-* I had a few meetup notes from tonight
 * Async helpful for IO, or slow processes
 * Async can help with responsiveness
 * A task is encapsulation of a background thread
